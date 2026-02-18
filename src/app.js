a25c4229abbb60bd97f4315e063db9f37e63ab8f
riyaz-shaik-16 <sr308379@gmail.com>
2026-02-18 11:27:20 +0530

    feat: update CORS configuration to include additional client URLs

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "2mb" }));
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL1, process.env.CLIENT_URL2],
+    origin: [process.env.CLIENT_URL1, process.env.CLIENT_URL2, process.env.CLIENT_URL3, process.env.CLIENT_URL4],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
